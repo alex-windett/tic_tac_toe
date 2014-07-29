632956466112bd22d20d317adcfb050a854a0950
Alex Windett <alexwindett@hotmail.com>
2014-07-29 22:11:29 +0100

    working on grid

--- tic_tac_toe/app/controllers/game_controller.rb
+++ tic_tac_toe/app/controllers/game_controller.rb
@@ -1,6 +1,5 @@
 class GameController < ApplicationController
  
-
   def index
     @move = Move.all
     @new_move = Move.new
@@ -8,9 +7,9 @@ class GameController < ApplicationController
 
     # @game = Game.create
 
-    if params[:player_move]
-      @game.moves.create(square_id:params[:player_move])
-    end
+  #   if params[:themove]
+  #     @game.moves.create(square_id:params[:themove])
+  #   end
   end
 
 
@@ -22,4 +21,46 @@ class GameController < ApplicationController
       @game.moves.create(square_id: params[:player_move])
     end
   end
+
+def new 
+ @game = Game.new(params[:game])
+  
+
+  respond_to do |format|
+      if @game.save
+        format.html { redirect_to @game, notice: 'Start Playing a new game.' }
+        format.json { render json: @game, status: :created, location: @game }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+end
+
+def create
+  @game = Game.new(params[:game])
+  raise
+
+  respond_to do |format|
+      if @game.save
+        format.html { redirect_to @game, notice: 'Start Playing a new game.' }
+        format.json { render json: @game, status: :created, location: @game }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+
+end
+
+def destroy
+  @game = Game.find(params[:id])
+  @game.destroy
+
+  respond_to do |format|
+      format.html { redirect_to game_index_url }
+      format.json { head :no_content }
+    end
+end
+
 end
