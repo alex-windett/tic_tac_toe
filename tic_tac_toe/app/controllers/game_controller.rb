b11638816006e643d3cf1e5425db2c5224d59d7d
Alex Windett <alexwindett@hotmail.com>
2014-07-29 16:28:49 +0100

    saving moves

--- tic_tac_toe/app/controllers/game_controller.rb
+++ tic_tac_toe/app/controllers/game_controller.rb
@@ -4,11 +4,22 @@ class GameController < ApplicationController
   def index
     @move = Move.all
     @new_move = Move.new
+    @game = Game.all
+
+    # @game = Game.create
+
+    if params[:player_move]
+      @game.moves.create(square_id:params[:player_move])
+    end
+  end
 
 
+  def show
+    @game = Game.find(params[:id])
+    @move = @game.moves
     
-    if params[:move]
-      raise
+    if params[:player_move]
+      @game.moves.create(square_id: params[:player_move])
     end
   end
 end
